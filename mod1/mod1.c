866a7ee770a2ca63ab6a162cf6dafc774365ed33
Angad Chauhan <angadchauhan102@gmail.com>
2026-05-01 21:03:43 +0530

    mod1: updated driver logic in mod1.c

--- mod1/mod1.c
+++ mod1/mod1.c
@@ -14,6 +14,7 @@ static int __init my_mod_init(void)
         pr_info("\033[1;33mThis is YELLOW message\033[0m\n");
 	pr_info("Enjoy this journey with the dedication\n");
 	pr_info("Your Github has been started\n");
+	pr_info("Your Github has been started\n");
 	return 0;
 }
 
